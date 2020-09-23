62fbbd14641cbe3e07c8b139e4dfc7d33efe3516
Can Tecim <can.tecim@gmail.com>
2020-09-23 23:18:43 +0300

    lint fixes

--- libs/validations/src/decorators/is-ISO6391.decorator.ts
+++ libs/validations/src/decorators/is-ISO6391.decorator.ts
@@ -2,7 +2,7 @@ import {registerDecorator, ValidationOptions} from "class-validator";
 import { isISO6391 } from "../validations";
 
 export function IsISO6391(validationOptions?: ValidationOptions) {
-  return function (object: Record<string, any>, propertyName: string) {
+  return function (object: Record<string, any>, propertyName: string): void {
     registerDecorator({
       name: "isISO639-1",
       target: object.constructor,
--- libs/validations/src/decorators/is-locale-code.decorator.ts
+++ libs/validations/src/decorators/is-locale-code.decorator.ts
@@ -2,7 +2,7 @@ import {registerDecorator, ValidationOptions} from "class-validator";
 import { isLocaleCode } from "../validations/is-locale-code";
 
 export function IsLocaleCode(validationOptions?: ValidationOptions) {
-  return function (object: Record<string, any>, propertyName: string) {
+  return function (object: Record<string, any>, propertyName: string): void {
     registerDecorator({
       name: "isLocaleCoe",
       target: object.constructor,
